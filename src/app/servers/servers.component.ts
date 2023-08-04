3cd7bf03d2d491ab60045316e0c71c6ac5697766
Gul Jamal Zim <guljamalzim@gmail.com>
2023-08-04 16:03:25 +0600

    Before starting excercise #2

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,10 +7,20 @@ import { Component } from '@angular/core';
 })
 export class ServersComponent {
   allowNewServers = false;
+  serverCreationStatus = 'No server was created';
+  serverName = 'Test server';
 
   constructor() {
     setTimeout(() => {
       this.allowNewServers = true;
     }, 2000);
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = `Server was created. Name is ${this.serverName}`;
+  }
+
+  onUpdateServeerName(event: any) {
+    this.serverName = (event.target as HTMLInputElement).value;
   }
 }
