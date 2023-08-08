139c8db9af41563dad76928169f25d0ea89aa34f
Gul Jamal Zim <guljamalzim@gmail.com>
2023-08-08 21:46:42 +0600

    Finish cmp-databinding excercise

--- cmp-databinding/src/app/game/game.component.ts
+++ cmp-databinding/src/app/game/game.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-game',
-  templateUrl: './game.component.html'
+  templateUrl: './game.component.html',
+  styleUrls: [
+    'game.component.css'
+  ]
 })
 export class GameComponent {
+  components: { type: "odd" | "even"; roundNum: number }[] = [];
 
+  onGameStarted(roundNum: number) {
+    this.components.push({
+      type: roundNum % 2 === 0 ? "even" : "odd",
+      roundNum: roundNum
+    })
+  }
 }
--- cmp-databinding/src/app/game/odd/odd.component.ts
+++ cmp-databinding/src/app/game/odd/odd.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-odd',
-  templateUrl: './odd.component.html'
+  templateUrl: './odd.component.html',
+  styles: [`p { background: blue; color: white; padding: 5px 10px }`]
 })
 export class OddComponent {
-
+  @Input() roundNum!: string;
 }
