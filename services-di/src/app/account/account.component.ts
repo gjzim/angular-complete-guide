2ab199344172203a007077968f0d09d44d3a1637
Gul Jamal Zim <guljamalzim@gmail.com>
2023-08-10 15:38:41 +0600

    Services DI chapter end

--- services-di/src/app/account/account.component.ts
+++ services-di/src/app/account/account.component.ts
@@ -1,9 +1,12 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { LoggingService } from '../logging.service';
+import { AccountService } from '../accounts.service';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.css'],
+  providers: [LoggingService],
 })
 export class AccountComponent {
   @Input() account!: { name: string; status: string };
@@ -13,8 +16,14 @@ export class AccountComponent {
     newStatus: string;
   }>();
 
+  constructor(
+    private loggingService: LoggingService,
+    private accountService: AccountService
+  ) {}
+
   onSetTo(status: string) {
-    this.statusChanged.emit({ id: this.id, newStatus: status });
-    console.log('A server status changed, new status: ' + status);
+    this.accountService.updateStatus(this.id, status);
+    // this.loggingService.logStatusChange(status);
+    this.accountService.statusUpdated.emit(status);
   }
 }
--- services-di/src/app/new-account/new-account.component.ts
+++ services-di/src/app/new-account/new-account.component.ts
@@ -1,18 +1,27 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { LoggingService } from '../logging.service';
+import { AccountService } from '../accounts.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
+  providers: [LoggingService],
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+  @Output() accountAdded = new EventEmitter<{ name: string; status: string }>();
+
+  constructor(
+    private loggingService: LoggingService,
+    private accountService: AccountService
+  ) {
+    this.accountService.statusUpdated.subscribe((status: string) => {
+      console.log('New status from new account ' + status);
+    });
+  }
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
-    console.log('A server status changed, new status: ' + accountStatus);
+    this.accountService.addAccount(accountName, accountStatus);
+    // this.loggingService.logStatusChange(accountStatus);
   }
 }
