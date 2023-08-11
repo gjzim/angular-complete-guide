3313e1bdf722bb63e00bdd6214c0e2b00a5b5a97
Gul Jamal Zim <guljamalzim@gmail.com>
2023-08-11 11:50:20 +0600

    Implement add to shopping list button functionality

--- course-project/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ course-project/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { ShoppingListService } from 'src/app/shopping-list/shopping-list.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -7,4 +8,12 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeDetailComponent {
   @Input() recipe!: Recipe;
+
+  constructor(private shoppingListService: ShoppingListService) {}
+
+  onAddToShoppingList(event: Event) {
+    event.preventDefault();
+
+    this.shoppingListService.addIngredients(this.recipe.ingredients);
+  }
 }
