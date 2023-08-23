27ee2a7d3925c0a65097d2cb1e86ee7974efc164
Gul Jamal Zim <guljamalzim@gmail.com>
2023-08-23 17:20:44 +0600

    Making Http Requests chapter WIP#1

--- http-requests/src/app/app.component.ts
+++ http-requests/src/app/app.component.ts
@@ -1,28 +1,58 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
+import { Post } from './post.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  loadedPosts = [];
+  loadedPosts: Post[] = [];
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
-  onCreatePost(postData: { title: string; content: string }) {
-    // Send Http request
-    console.log(postData);
+  onCreatePost(postData: Post) {
+    this.http
+      .post<{ name: string }>(
+        'https://angular-complete-guide-61789-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json',
+        postData
+      )
+      .subscribe((data) => console.log(data));
   }
 
   onFetchPosts() {
-    // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private fetchPosts() {
+    this.http
+      .get<{ [key: string]: Post }>(
+        'https://angular-complete-guide-61789-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json'
+      )
+      .pipe(
+        map((responseData) => {
+          const postsArray: Post[] = [];
+          for (const key in responseData) {
+            postsArray.push({
+              id: key,
+              ...responseData[key],
+            });
+          }
+          return postsArray;
+        })
+      )
+      .subscribe((posts) => {
+        this.loadedPosts = posts;
+      });
   }
 }
