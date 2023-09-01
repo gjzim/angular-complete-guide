c81620bbef8d075169cb103685c6d7fc68adce49
Gul Jamal Zim <guljamalzim@gmail.com>
2023-09-01 17:18:43 +0600

    Create template project

---
+++ template/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Subscription, map } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  constructor() {}
+}
