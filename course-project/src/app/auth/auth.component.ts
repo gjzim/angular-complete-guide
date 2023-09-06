3ac34e91a9caa50dd883da93cfc37789991376a9
Gul Jamal Zim <guljamalzim@gmail.com>
2023-09-06 18:26:04 +0600

    Complete 299th video

--- course-project/src/app/auth/auth.component.ts
+++ course-project/src/app/auth/auth.component.ts
@@ -1,44 +1,51 @@
-import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { AuthService } from './auth.service';
+import {Component} from '@angular/core';
+import {NgForm} from '@angular/forms';
+import {AuthResponseData, AuthService} from './auth.service';
+import {Observable} from "rxjs";
 
 @Component({
-  selector: 'app-auth',
-  templateUrl: './auth.component.html',
+    selector: 'app-auth',
+    templateUrl: './auth.component.html',
 })
 export class AuthComponent {
-  isLoginMode = true;
-  isLoading = false;
-  error = '';
+    isLoginMode = true;
+    isLoading = false;
+    error = '';
 
-  constructor(private authService: AuthService) {}
-
-  onSwitchMode() {
-    this.isLoginMode = !this.isLoginMode;
-  }
-
-  onSubmit(form: NgForm) {
-    if (form.invalid) {
-      return;
+    constructor(private authService: AuthService) {
     }
 
-    this.isLoading = true;
-
-    if (this.isLoginMode) {
-    } else {
-      this.authService.signup(form.value.email, form.value.password).subscribe({
-        next: (resData) => {
-          console.log(resData);
-          this.isLoading = false;
-        },
-        error: (error) => {
-          console.log(error);
-          this.error = error;
-          this.isLoading = false;
-        },
-      });
+    onSwitchMode() {
+        this.isLoginMode = !this.isLoginMode;
     }
 
-    form.reset();
-  }
+    onSubmit(form: NgForm) {
+        if (form.invalid) {
+            return;
+        }
+
+        this.isLoading = true;
+        this.error = '';
+        let authObservable: Observable<AuthResponseData>;
+
+        if (this.isLoginMode) {
+            authObservable = this.authService.login(form.value.email, form.value.password)
+        } else {
+            authObservable = this.authService.signup(form.value.email, form.value.password)
+        }
+
+        authObservable.subscribe({
+            next: (resData) => {
+                console.log(resData);
+                this.isLoading = false;
+            },
+            error: (error) => {
+                console.log(error);
+                this.error = error;
+                this.isLoading = false;
+            },
+        })
+
+        form.reset();
+    }
 }
--- course-project/src/app/auth/auth.service.ts
+++ course-project/src/app/auth/auth.service.ts
@@ -1,47 +1,64 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { catchError, throwError } from 'rxjs';
-import { environment } from '../../environments/environment';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {catchError, throwError} from 'rxjs';
+import {environment} from '../../environments/environment';
 
-interface AuthResponse {
-  kind: string;
-  idToken: string;
-  email: string;
-  refreshToken: string;
-  expiersIn: string;
-  localId: string;
+export interface AuthResponseData {
+    kind: string;
+    idToken: string;
+    email: string;
+    refreshToken: string;
+    expiresIn: string;
+    localId: string;
+    registered?: boolean;
 }
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class AuthService {
-  constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient) {
+    }
 
-  signup(email: string, password: string) {
-    return this.http
-      .post<AuthResponse>(
-        `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${environment.FIREBASE_API_KEY}`,
-        { email, password, returSecureToken: true }
-      )
-      .pipe(
-        catchError((errorRes) => {
-          let errorMessage = 'An unknown error occured!';
+    signup(email: string, password: string) {
+        return this.http
+            .post<AuthResponseData>(
+                `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${environment.FIREBASE_API_KEY}`,
+                {email, password, returnSecureToken: true}
+            )
+            .pipe(catchError(this.handleError));
+    }
 
-          if (!errorRes.error || !errorRes.error.error) {
+    login(email: string, password: string) {
+        return this.http
+            .post<AuthResponseData>(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.FIREBASE_API_KEY}`, {
+                email,
+                password,
+                returnSecureToken: true
+            })
+            .pipe(catchError(this.handleError));
+    }
+
+    private handleError(errorRes: HttpErrorResponse) {
+        let errorMessage = 'An unknown error occurred!';
+
+        if (!errorRes.error || !errorRes.error.error) {
             return throwError(errorMessage);
-          }
+        }
 
-          switch (errorRes.error.error.message) {
+        switch (errorRes.error.error.message) {
             case 'EMAIL_EXISTS':
-              errorMessage = 'This email exists already!';
-              break;
-
+                errorMessage = 'This email exists already!';
+                break;
+            case 'EMAIL_NOT_FOUND':
+                errorMessage = 'This email does not exist!';
+                break;
+            case 'INVALID_PASSWORD':
+                errorMessage = 'This password is not correct!';
+                break;
             default:
-              break;
-          }
-          return throwError(errorMessage);
-        })
-      );
-  }
+                break;
+        }
+        return throwError(errorMessage);
+    }
 }
