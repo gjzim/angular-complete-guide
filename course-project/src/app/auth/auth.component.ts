999c757493fdde204fb27feda186b3b493541509
Gul Jamal Zim <guljamalzim@gmail.com>
2023-09-18 12:07:11 +0600

    Dyanic components chapter complete

--- course-project/src/app/auth/auth.component.ts
+++ course-project/src/app/auth/auth.component.ts
@@ -1,18 +1,27 @@
-import { Component, ComponentFactoryResolver } from '@angular/core';
+import {
+  Component,
+  ComponentFactoryResolver,
+  OnDestroy,
+  ViewChild,
+} from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthResponseData, AuthService } from './auth.service';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { Router } from '@angular/router';
 import { AlertComponent } from '../shared/alert/alert.component';
+import { PlaceholderDirective } from '../shared/placeholder/placeholder.directive';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
 })
-export class AuthComponent {
+export class AuthComponent implements OnDestroy {
   isLoginMode = true;
   isLoading = false;
   error = '';
+  @ViewChild(PlaceholderDirective, { static: false })
+  alertHost!: PlaceholderDirective;
+  private closeSub!: Subscription;
 
   constructor(
     private authService: AuthService,
@@ -66,8 +75,23 @@ export class AuthComponent {
     this.error = '';
   }
 
-  showErrorAlert(error: string) {
-    const alertComponent =
+  private showErrorAlert(error: string) {
+    const alertCmpFactory =
       this.componentFactoryResolver.resolveComponentFactory(AlertComponent);
+    const hostViewContainerRef = this.alertHost.viewContainerRef;
+    hostViewContainerRef.clear();
+    const cmpRef = hostViewContainerRef.createComponent(alertCmpFactory);
+
+    cmpRef.instance.message = error;
+    this.closeSub = cmpRef.instance.close.subscribe(() => {
+      this.closeSub.unsubscribe();
+      hostViewContainerRef.clear();
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.closeSub) {
+      this.closeSub.unsubscribe();
+    }
   }
 }
