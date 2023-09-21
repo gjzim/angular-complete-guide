4c9158c3714c983a22ddce6a03733e1f268cc691
Gul Jamal Zim <guljamalzim@gmail.com>
2023-09-21 17:04:43 +0600

    Add shared module

--- course-project/src/app/app.module.ts
+++ course-project/src/app/app.module.ts
@@ -5,19 +5,17 @@ import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RecipeService } from './recipes/recipe.service';
 import { AuthComponent } from './auth/auth.component';
-import { AuthIntercptorService } from './auth/auth-interceptor.service';
-import { AlertComponent } from './shared/alert/alert.component';
 import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core.module';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, AuthComponent],
   imports: [
+    CoreModule,
     SharedModule,
     BrowserModule,
     AppRoutingModule,
@@ -26,15 +24,6 @@ import { SharedModule } from './shared/shared.module';
     ReactiveFormsModule,
     RecipesModule,
     ShoppingListModule,
-  ],
-  providers: [
-    ShoppingListService,
-    RecipeService,
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthIntercptorService,
-      multi: true,
-    },
   ],
   bootstrap: [AppComponent],
 })
