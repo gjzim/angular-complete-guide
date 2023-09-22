9f6d278c067e7ca7817771072cfba55ba356df70
Gul Jamal Zim <guljamalzim@gmail.com>
2023-09-22 17:58:01 +0600

    Implement lazy loading for recipes module

--- course-project/src/app/app-routing.module.ts
+++ course-project/src/app/app-routing.module.ts
@@ -5,6 +5,11 @@ import { AuthComponent } from './auth/auth.component';
 const routes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
   { path: 'auth', component: AuthComponent },
+  {
+    path: 'recipes',
+    loadChildren: () =>
+      import('./recipes/recipes.module').then((m) => m.RecipesModule),
+  },
 ];
 
 @NgModule({
--- course-project/src/app/app.module.ts
+++ course-project/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FormsModule } from '@angular/forms';
-import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
@@ -21,7 +20,6 @@ import { AuthModule } from './auth/auth.module';
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
-    RecipesModule,
     ShoppingListModule,
     AuthModule,
   ],
